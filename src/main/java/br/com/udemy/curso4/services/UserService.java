c698928fd73f569e3e73e88dd11be54e24bb5779
Marcio Antunes <antunes-marcio@hotmail.com>
2019-11-14 15:50:13 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/udemy/curso4/services/UserService.java
@@ -0,0 +1,24 @@
+package br.com.udemy.curso4.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.udemy.curso4.entities.User;
+import br.com.udemy.curso4.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public	List<User> findAll() {
+		return repository.findAll();
+	}
+	public	User findById (Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
