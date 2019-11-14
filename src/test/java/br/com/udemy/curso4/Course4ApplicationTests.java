44bd3c6533dfd461ad187fbbfed95d63da537330
Marcio Antunes <antunes-marcio@hotmail.com>
2019-11-14 09:04:06 -0300

    User entity and resource

---
+++ src/test/java/br/com/udemy/curso4/Course4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.curso4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
